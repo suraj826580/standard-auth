7495a763ee2839efaaed4c22ad7a7e939da2be97
Suraj Yadav <111379728+suraj826580@users.noreply.github.com>
2024-02-02 21:59:07 +0530

    intial commit

--- another_oauth/server/server.js
+++ another_oauth/server/server.js
@@ -1,3 +1,5 @@
+// we are checking here the version of node
+
 const [major, minor] = process.versions.node.split(".").map(Number);
 if (major < 10) {
   console.log(
@@ -5,9 +7,26 @@ if (major < 10) {
   );
   process.exit();
 }
+// Application is Start fro here
 import express from "express";
 import "dotenv/config";
 const app = express();
+
+app.get("/", async function (req, res) {
+  try {
+    res.status(200).send({ msg: "Home Page" });
+  } catch (error) {
+    res.status(400).send({ msg: error });
+  }
+});
+
+app.get("/login", async (req, res) => {
+  try {
+    res.status(200).send({ msg: "Login Successfull" });
+  } catch (error) {
+    res.status(400).send({ msg: error });
+  }
+});
 
 app.set("port", process.env.PORT || 8765);
 const server = app.listen(app.get("port"), () => {
